33940e65e3bf4b9c50d3ceec4e59f359f6f94611
Cyberdrick <rodrigoschort01@gmail.com>
2022-11-30 10:43:16 -0300

    1.0

---
+++ src/pages/WriteNowPage.tsx
@@ -0,0 +1,39 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import { Button, DateTimePicker, Nav, RichInput } from "components";
+
+export function WriteNowPage() {
+    const formMethods = useForm();
+    const { register, handleSubmit} = formMethods
+
+    function onSubmit(values: any) {
+        console.log(
+            ' ~ file: WriteNowPage.tsx ~ line 9 ~ onSubmit ~ values',
+            values
+        );
+    }
+
+    return (
+        <>
+            <Nav />
+            <div className="container">
+                <h1>Escrever Agora</h1>
+
+                <FormProvider {...formMethods}>
+                    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                        <label htmlFor="destinationName">Nome Completo</label>
+                        <input {...register('destinationName')} type="text" />
+                        <label htmlFor="destinationAddress">E-mail</label>
+                        <input {...register('destinationAddress')} type="email" />
+                        <label htmlFor="dueDate">Data</label>
+                        <DateTimePicker name="dueDate"/>
+                        <label htmlFor="subject">Assunto</label>
+                        <input {...register('subject')} type="text" />
+                        <label htmlFor="body">Mensagem</label>
+                        <RichInput name="body" />
+                        <Button type="submit" variant="primary">Enviar</Button>
+                    </form>
+                </FormProvider>
+            </div>
+        </>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primaryLight: '#7dd3fc',
+        primary: '#0284c7',
+        primaryDark: '#164e63',
+      },
+    },
+  },
+  plugins: [],
+};
